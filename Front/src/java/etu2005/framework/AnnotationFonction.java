e31f844d2b67939725341ae53d7e551c6a6e6e06
MendrikaValisoa <mendrikavalisoa721@gmail.com>
2023-03-26 20:16:02 +0300

    sprint 3 tena izy

--- Front/src/java/etu2005/framework/AnnotationFonction.java
+++ Front/src/java/etu2005/framework/AnnotationFonction.java
@@ -17,25 +17,29 @@ import java.util.List;
  */
 public class AnnotationFonction {
     public static List<Class<?>> getModelClasses(String package_name){
-        List<Class<?>> classes = new ArrayList<>();
-       try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            Enumeration<URL> resources = classLoader.getResources(package_name);
-            while(resources.hasMoreElements()){
-                URL res = resources.nextElement();
-                if(res.getProtocol().equals("file")){
-                    File packageDir = new File(res.toURI());
-                    for(File file : packageDir.listFiles()){
-                        String filename = file.getName();
-                        if(filename.endsWith(".class")){
-                            String className = filename.substring(0, filename.length()-6);
-                            System.out.println(className);
-                            Class<?> c = Class.forName(package_name + "." + className);
-                        }
+        List<Class<?>> classes= new ArrayList<>();
+        try {
+            ClassLoader classLoader= Thread.currentThread().getContextClassLoader();
+            Enumeration<URL> resources= classLoader.getResources(package_name);
+            while(resources.hasMoreElements()) {
+                URL res= resources.nextElement();
+                if(res.getProtocol().equals("file")) {
+                    File packageDir= new File(res.toURI());
+                    for(File file : packageDir.listFiles()) {
+                            String filename= file.getName();
+                            if(filename.endsWith(".class")) {
+                                    String className= filename.substring(0, filename.length()-6);
+                                    System.out.println(className);
+                                    Class<?> c= Class.forName(package_name + "." + className);
+                                    classes.add(c);
+                            }
                     }
                 }
             }
-        } catch (Exception e) {}
-       return classes;
-   }
+
+        } catch (Exception e) {
+                System.out.println(e);
+        }
+		return classes;
+	}
 }
--- Front/src/java/etu2005/framework/servlet/Frontservlet.java
+++ Front/src/java/etu2005/framework/servlet/Frontservlet.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Frontservlet extends HttpServlet {
     Map<String,Mapping> MappingUrls = new HashMap<>();
     
- 
+    @Override
     public String getServletInfo() {
         return "Short description";
     }
@@ -43,20 +43,35 @@ public class Frontservlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+               throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+                     MappingUrls=FonctionUrl.fonction();
+                     System.out.println("huhu");
+         
+         for(Map.Entry<String,Mapping> u : MappingUrls.entrySet())
+         {
+             System.out.println("jiji");
+             System.out.println(u.getKey() +  " : "  +  u.getValue().getClassName()+ " || " +  u.getValue().getMethod());
+         }
             /* TODO output your page here. You may use following sample code. */
-            MappingUrls = FonctionUrl.fonction();
-            System.out.println("test");
-        for(Map.Entry<String,Mapping> u : MappingUrls.entrySet()){
-            System.out.println("huhu");
-            System.out.println(u.getKey() + " : " + u.getValue().getClassName()+ " || " + u.getValue().getMethod());
-        }
             out.println(request.getContextPath()+request.getServletPath()+ "?" +request.getQueryString());
         }
-        }
     }
+    
+      @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+}
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -67,12 +82,8 @@ public class Frontservlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
+   
+
 //    /**
 //     * Handles the HTTP <code>POST</code> method.
 //     *
@@ -81,11 +92,7 @@ public class Frontservlet extends HttpServlet {
 //     * @throws ServletException if a servlet-specific error occurs
 //     * @throws IOException if an I/O error occurs
 //     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);::
-//    }
+  
 
      /**
      * Returns a short description of the servlet.
